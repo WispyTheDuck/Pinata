f909d1024ce7b88b9a2d68c76829d892a0ca2972
WispyTheDuck <57881224+WispyTheDuck@users.noreply.github.com>
2020-02-09 18:40:44 -0800

    Update Economy.py

--- Pinata/cogs/Economy.py
+++ Pinata/cogs/Economy.py
@@ -9,19 +9,6 @@ class Economy(commands.Cog):
     def __innit__(self, bot):
         self.bot = bot
 
-
-    @commands.command()
-    async def work(self, ctx):
-        with open('users.json', "r") as f:
-            if user.id not in ['users.json']:
-                user = json.load(f)
-
-            userID = (f"{user.id}"),
-            wallet = 0,
-            bank = 0
-
-            with open('users.json', 'w') as f:
-                json.dump(user, f, indent=4)
              
 
 def setup(bot):
